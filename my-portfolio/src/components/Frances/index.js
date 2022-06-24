362fe2670272456da5d155bd64cfc6f5fb7af18f
Frances Huang <frances.y.huang@gmail.com>
2022-06-23 21:14:07 -0700

    push

--- my-portfolio/src/components/Frances/index.js
+++ my-portfolio/src/components/Frances/index.js
@@ -1,5 +1,12 @@
+import ascii from "../../assets/images/frances-ascii.svg";
+
 const Frances = () => {
-	return <div className="page">Frances</div>;
+	return (
+		<div className="page">
+			Frances
+			<img src={ascii} className="ascii-art" alt="ASCII" />
+		</div>
+	);
 };
 
 export default Frances;
